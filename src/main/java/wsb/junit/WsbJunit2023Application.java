e64eeefe52a6a181ea7106bc9d66641c5acdc60b
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-04-01 10:47:37 +0200

    init

---
+++ src/main/java/wsb/junit/WsbJunit2023Application.java
@@ -0,0 +1,13 @@
+package wsb.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbJunit2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbJunit2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/junit/WsbJunit2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbJunit2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
